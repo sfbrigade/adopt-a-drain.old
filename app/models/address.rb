d731947a81440d018b7b96984574f43a56c88b6c
Eddie A Tejeda <eddie@civicindustries.com>
2013-11-22 18:02:40 -0800

    use new google geocoder

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address
   include Geokit::Geocoders
 
   def self.geocode(address)
-    Geokit::Geocoders::GoogleGeocoder.geocode(address).ll.split(',').map{|s| s.to_f}
+    Geokit::Geocoders::GoogleGeocoder3.geocode(address).ll.split(',').map{|s| s.to_f}
     # MultiGeocoder.geocode(address).ll.split(',').map{|s| s.to_f}
   end
 end
---
+++ config/initializers/geokit.rb
@@ -0,0 +1,2 @@
+Geokit::Geocoders::Google3Geocoder = Geokit::Geocoders::GoogleGeocoder3
+Geokit::Geocoders::provider_order = [:google3]
